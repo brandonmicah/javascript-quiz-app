be594e3a18e6ebe1226c7a881986c999bcc7635a
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-04-26 09:29:48 +0100

    Very HTML & CSS done

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+/* Define an array of objects that contain the quiz questions, answer choices, and correct answer.
+Write a function to display the questions and answer choices from the array onto the webpage.
+Write a function to check the user's answer and display the result.
+Write a function to keep track of the user's score.
+*/
